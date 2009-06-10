ed8a1dc31077c16646a7446d09a6c76ecf0fd5e0
Sander Dijkhuis <sander.dijkhuis@gmail.com>
2009-06-11 00:15:59 +0200

    Add Alberto to the list of contributors

--- docs/docs.js
+++ docs/docs.js
@@ -20,6 +20,7 @@
  * Contributor(s):
  *   Atul Varma <atul@mozilla.com>
  *   Sander Dijkhuis <sander.dijkhuis@gmail.com>
+ *   Alberto Santini <albertosantini@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
